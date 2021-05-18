d2c32fdf80d714e723fb013014984ad6ab1fce5d
Gabriel Checchia Vitali <vitali.gabriel@gmail.com>
2021-05-18 20:51:56 +0200

    Fixing tests
    - Removed tests that depend on constant values and API keys that are not allowed to be uploaded to a public git repo.
    
    Ideally a Unit test shouldn't test if another API is working or not, it should only check whether the library is in knows how to deal with the data it returns and possible errors. That's why I'll add more tests in the future which cover these scenarios.
    
    Currently the bot doesn't do much, it only runs the request and returns the response, that's why I'll add more tests in the future, when some more fancy treatments are in.

--- src/wordpress-poster/wordpress-poster-controller.test.js
+++ src/wordpress-poster/wordpress-poster-controller.test.js
@@ -12,28 +12,4 @@ describe("test if wordpress REST API from address is reachable", () => {
     expect(response[0].link).toBeTruthy()
   });
 
-  it("should retrieve valid posts from the blog inserted in the constants", async() => {
-    const response = await getAllPosts()
-    expect(response.length).not.toBe(0)
-    expect(response[0].id).toBeTruthy()
-    expect(response[0].date_gmt).toBeTruthy()
-    expect(response[0].date).toBeTruthy()
-    expect(response[0].slug).toBeTruthy()
-    expect(response[0].link).toBeTruthy()
-  });
-
-  it("should create a post without issues", async() => {
-    let body = {
-      status: 'draft',
-      title: 'New Test Post',
-      content: "Howdy!",
-    }
-
-    const response = await createNewPost(body)
-    expect(response.code).toBeUndefined()
-    expect(response.message).toBeUndefined()
-    expect(response.id).toBeTruthy()
-    expect(response.date_gmt).toBeTruthy()
-    expect(response.generated_slug).toBeTruthy()
-  });
 });
